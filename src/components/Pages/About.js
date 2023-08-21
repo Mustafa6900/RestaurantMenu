bebb200ef218d0eb8bdb11ead81de7ca8c94d994
MustafaDemirhan <58307398+Mustafa6900@users.noreply.github.com>
2023-08-21 20:24:07 +0300

    Update About.js

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -8,7 +8,7 @@ export const About = () => {
       <br />
       <div className="hakkimizda">
      
-      1978’de Baybur’ta başlayan esnaflığımızdan ödün vermeden; 
+      1978’de Bayburt’da başlayan esnaflığımızdan ödün vermeden; 
       Bayburt et-balık ve restoran sektörünün lider firması olmak, Bayburt'un önde gelen restoran markalarından biri olup memleketimize kazanç getirmek.” vizyonuyla hareket eden Dombili Kebap, 
       Müşteri Odaklılık, Bireylere Saygı, Rekabetçi Düşünce ve Eylem olmak üzere üç ana kategoride benimsediği ve çok önem verdiği değerlerini takip ederek yolculuğuna devam eden firma, işini tutkuyla yapan, güçlü bir ailedir.
       <br />
